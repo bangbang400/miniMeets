aaa7707b999f137c03916f48a37ab9fb3b43a455
Yamato Baba <baba@infocity.co.jp>
2022-09-05 20:38:43 +0900

    test2

---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:meets_on_flutter/LoginPage.dart';
+import 'BottomTabPage.dart';
+import 'SplashPage.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+import 'firebase_options.dart';
+
+// first commit
+// git push test
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform
+  );
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Meets',
+      // home: LoginPage(),
+      // home: SplashPage(),
+      home: BottomTabPage(),
+      // routes: <String, WidgetBuilder>{
+      //   '/login': (BuildContext context) => LoginPage(),
+      //   '/friends': (BuildContext context) => FriendsTabPage(),
+      // },
+    );
+  }
+}
+
